9ab206572044acbd0c54fe776ee68c308adafafe
bishnt <bishrant150@gmail.com>
2025-06-01 13:46:57 +0545

    todo list

--- React/todoList/todoList/src/App.jsx
+++ React/todoList/todoList/src/App.jsx
@@ -3,33 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    title: "Go to gym",
+    description: "Go to gym from 7-9",
+    completed: false
+  }, {
+    title: "study physics",
+    description: "study Physics from 9-10",
+    completed: true
+  },
+  {
+    title: "study math",
+    description: "study math from 9-10",
+    completed: true
+  }]);
+
+  function addTodo(){
+    setTodos([...todos, {
+      title: "new Todo",
+      description: "desc of new todo"
+    }])
+  }
 
   return (
     <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={addTodo}>Add a Todo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {todos.map(function(todo){
+        return <Todo title = {todo.title} description = {todo.description} />
+      })}
+    </div>
+
     </>
   )
+}
+
+
+function Todo(props) {
+  return <div>
+    <h1>{props.title}</h1>
+    <p>{props.description}</p>
+  </div>
 }
 
 export default App
